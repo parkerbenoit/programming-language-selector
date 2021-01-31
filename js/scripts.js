971577a99a812143e778f74200462efcb931a2a8
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2021-01-30 20:15:24 -0800

    have 5 questions for user to answer and a retake quiz button that reloads page

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     event.preventDefault();
     const question1 = parseInt($("input#question1").val());
     const question2 = parseInt($("input#question2").val());
+    const question3 = parseInt($("input#question3").val());
+    const question4 = parseInt($("input#question4").val());
+    const question5 = parseInt($("input#question5").val());
     $("#selector").toggle();
 
     if (question1 === question2) {
@@ -12,3 +15,8 @@ $(document).ready(function() {
     }
   });
 });
+
+
+function refreshPage() {
+  window.location.reload();
+}
